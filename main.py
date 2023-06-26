9dc26d6f97473c471322ea1f77da6c45dcc3f068
brianchancb <60330336+brianchancb@users.noreply.github.com>
2023-06-26 20:12:59 +0800

    Crypto Data Pull
    This commit tries to establish a data puller from the internet

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from pull_data import pull_data
 
 
 if __name__ == '__main__':
+    pull_data()
     print(pd.__version__)
 
 
---
+++ pull_data.py
@@ -0,0 +1,4 @@
+import requests
+
+def pull_data():
+    pass
