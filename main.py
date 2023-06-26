c08e6d8891643a836929b051bdcf54b3518b3cb1
brianchancb <60330336+brianchancb@users.noreply.github.com>
2023-06-26 20:26:18 +0800

    formatting

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import pandas as pd
 from pull_data import pull_data
 import numpy as np
 
-
-
 if __name__ == '__main__':
     pull_data()
     print(pd.__version__)
     print(np.random.randint(500))
-
-
